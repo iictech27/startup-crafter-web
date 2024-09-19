f19187a39afa7d01f5c43828230f778e122c2c4e
Anirban Dutta <meanirbandutta22@gmail.com>
2024-09-20 00:09:02 +0530

    redux persist added

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,9 +4,13 @@ import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./store/store.js";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistedStore } from "./store/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistedStore}>
+      <App />
+    </PersistGate>
   </Provider>
 );
--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,10 +1,24 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../features/users/userSlice";
 import adminReducer from "../features/users/adminSlice";
+import { persistReducer, persistStore } from "redux-persist";
+import { combineReducers } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const rootReducer = combineReducers({
+  user: userReducer,
+  admin: adminReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-    admin: adminReducer,
-  },
+  reducer: persistedReducer,
 });
+
+export const persistedStore = persistStore(store);
