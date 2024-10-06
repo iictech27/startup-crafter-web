9b370f38e75703d24f6e78a298b5dcb82b3f4d5e
Anirban Dutta <meanirbandutta22@gmail.com>
2024-10-07 01:22:36 +0530

    function for saving blog added to userSlice

--- client/src/features/users/userSlice.js
+++ client/src/features/users/userSlice.js
@@ -1,8 +1,32 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
   users: null,
+  loading: false,
+  error: null,
 };
+
+//save blogs
+export const saveBlog = createAsyncThunk(
+  "users/saveBlog",
+  async (data, { rejectWithValue }) => {
+    console.log(data);
+    const res = await axios.post("/api/v1/user/save-blog", data, {
+      withCredentials: true,
+      headers: {
+        Accept: "application/json",
+      },
+    });
+
+    try {
+      console.log(res.data);
+      return res.data.data;
+    } catch (error) {
+      return rejectWithValue(error.response.data || error.message);
+    }
+  }
+);
 
 export const userSlice = createSlice({
   name: "users",
@@ -10,11 +34,24 @@ export const userSlice = createSlice({
   reducers: {
     setUser: (state, action) => {
       state.users = action.payload;
-      // console.log(users);
     },
     clearUser: (state, action) => {
       state.users = null;
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(saveBlog.pending, (state) => {
+      state.loading = true;
+      state.error = null;
+    });
+    builder.addCase(saveBlog.fulfilled, (state, action) => {
+      state.loading = false;
+      state.users.savedBlogs.push(action.payload);
+    });
+    builder.addCase(saveBlog.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    });
   },
 });
 
