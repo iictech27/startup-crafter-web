fd5849f66e7851d28f5d781041b573d3ac3e8d7d
Anirban Dutta <meanirbandutta22@gmail.com>
2024-10-07 01:24:09 +0530

    userBlogSlice added

--- client/src/features/blog/userBlogSlice.js
+++ client/src/features/blog/userBlogSlice.js
@@ -1,25 +1,71 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
-  blogs: null,
+  blogs: [],
+  myBlogs: [],
+  savedBlogs: [],
+  loading: false,
+  error: null,
 };
+
+//fetch all blogs
+export const fetchAllBlogs = createAsyncThunk(
+  "blogs/fetchAllBlogs",
+  async (_, { rejectWithValue }) => {
+    const res = await axios.get("/api/v1/user/get-all-blogs", {
+      withCredentials: true,
+      headers: {
+        Accept: "application/json",
+      },
+    });
+
+    try {
+      console.log(res.data);
+      return res.data.data;
+    } catch (error) {
+      return rejectWithValue(error.response.data || error.message);
+    }
+  }
+);
 
 export const userBlogSlice = createSlice({
   name: "blogs",
   initialState,
   reducers: {
-    createBlog: (state, action) => {
+    fetchUserCreatedBlogs: (state, action) => {
+      state.myBlogs = state.blogs.filter(
+        (blog) => action.payload === blog.createdBy._id
+      );
+    },
+    fetchUserSavedBlogs: (state, action) => {
+      state.savedBlogs = state.blogs.filter((blog) =>
+        action.payload.includes(blog._id)
+      );
+    },
+    clearBlogs: (state, action) => {
+      state.blogs = [];
+      state.savedBlogs = [];
+      state.myBlogs = [];
+    },
+  },
+  extraReducers: (builder) => {
+    // handling fetch all blogs
+    builder.addCase(fetchAllBlogs.pending, (state) => {
+      state.loading = true;
+      state.error = null;
+    });
+    builder.addCase(fetchAllBlogs.fulfilled, (state, action) => {
+      state.loading = false;
       state.blogs = action.payload;
-      // console.log(users);
-    },
-    editBlog: (state, action) => {
-      state.blogs = action.payload;
-    },
-    deleteBlog: (state, action) => {
-      state.blogs = null;
-    },
+    });
+    builder.addCase(fetchAllBlogs.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    });
   },
 });
 
-export const { createBlog, editBlog, deleteBlog } = userBlogSlice.actions;
+export const { fetchUserCreatedBlogs, fetchUserSavedBlogs, clearBlogs } =
+  userBlogSlice.actions;
 export default userBlogSlice.reducer;
