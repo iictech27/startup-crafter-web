b0f66fe288ca0095901513b3349838908e2b7ca5
Anirban Dutta <meanirbandutta22@gmail.com>
2024-10-15 16:47:51 +0530

    more functions added

--- client/src/features/blog/userBlogSlice.js
+++ client/src/features/blog/userBlogSlice.js
@@ -3,11 +3,33 @@ import axios from "axios";
 
 const initialState = {
   blogs: [],
+  blogDetails: null,
   myBlogs: [],
   savedBlogs: [],
   loading: false,
   error: null,
 };
+
+//create blog
+export const createBlog = createAsyncThunk(
+  "users/createBlog",
+  async (data, { rejectWithValue }) => {
+    console.log(data);
+    const res = await axios.post("/api/v1/user/create-blog", data, {
+      withCredentials: true,
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+
+    try {
+      console.log(res.data);
+      return res.data.data;
+    } catch (error) {
+      return rejectWithValue(error.response.data || error.message);
+    }
+  }
+);
 
 //fetch all blogs
 export const fetchAllBlogs = createAsyncThunk(
@@ -29,27 +51,81 @@ export const fetchAllBlogs = createAsyncThunk(
   }
 );
 
+//fetch individual blog from database
+export const fetchIndividualBlog = createAsyncThunk(
+  "blogs/fetchIndividualBlog",
+  async (individualBlogSlug, { rejectWithValue }) => {
+    const res = await axios.get(`/api/v1/user/get-blog/${individualBlogSlug}`, {
+      withCredentials: true,
+      headers: {
+        Accept: "application/json",
+      },
+    });
+
+    try {
+      console.log(res.data);
+      return res.data.data;
+    } catch (error) {
+      return rejectWithValue(error.response.data || error.message);
+    }
+  }
+);
+
 export const userBlogSlice = createSlice({
   name: "blogs",
   initialState,
   reducers: {
     fetchUserCreatedBlogs: (state, action) => {
       state.myBlogs = state.blogs.filter(
-        (blog) => action.payload === blog.createdBy._id
+        (blog) => action.payload === blog.createdBy.uuid
       );
     },
     fetchUserSavedBlogs: (state, action) => {
       state.savedBlogs = state.blogs.filter((blog) =>
-        action.payload.includes(blog._id)
+        action.payload.includes(blog.uuid)
+      );
+    },
+    //fetch individual blogs from redux state
+    fetchIndividualBlogRedux: (state, action) => {
+      state.blogDetails = state.blogs.find(
+        (blog) => action.payload === blog.slug
       );
     },
     clearBlogs: (state, action) => {
-      state.blogs = [];
       state.savedBlogs = [];
       state.myBlogs = [];
+      state.blogDetails = null;
     },
   },
   extraReducers: (builder) => {
+    //handle create blog
+    builder.addCase(createBlog.pending, (state) => {
+      state.loading = true;
+      state.error = null;
+    });
+    builder.addCase(createBlog.fulfilled, (state, action) => {
+      state.loading = false;
+      state.blogs.push(action.payload);
+    });
+    builder.addCase(createBlog.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    });
+
+    //handling fetch individual blog
+    builder.addCase(fetchIndividualBlog.pending, (state) => {
+      state.loading = true;
+      state.error = null;
+    });
+    builder.addCase(fetchIndividualBlog.fulfilled, (state, action) => {
+      state.loading = false;
+      state.blogDetails = action.payload;
+    });
+    builder.addCase(fetchIndividualBlog.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    });
+
     // handling fetch all blogs
     builder.addCase(fetchAllBlogs.pending, (state) => {
       state.loading = true;
@@ -66,6 +142,10 @@ export const userBlogSlice = createSlice({
   },
 });
 
-export const { fetchUserCreatedBlogs, fetchUserSavedBlogs, clearBlogs } =
-  userBlogSlice.actions;
+export const {
+  fetchUserCreatedBlogs,
+  fetchUserSavedBlogs,
+  fetchIndividualBlogRedux,
+  clearBlogs,
+} = userBlogSlice.actions;
 export default userBlogSlice.reducer;
