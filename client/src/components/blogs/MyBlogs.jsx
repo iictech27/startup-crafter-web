da4d0d822bbc52e8abc5adcb58c181875d0c818d
Anirban Dutta <meanirbandutta22@gmail.com>
2024-10-15 16:50:02 +0530

    user created blogs fetch functionality

--- client/src/components/blogs/MyBlogs.jsx
+++ client/src/components/blogs/MyBlogs.jsx
@@ -1,29 +1,50 @@
 import { useDispatch, useSelector } from "react-redux";
 import BlogsList from "./BlogList";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { fetchUserCreatedBlogs } from "../../features/blog/userBlogSlice";
+import { Link } from "react-router-dom";
 
 export default function MyBlogs() {
   const dispatch = useDispatch();
-  //fetching user id
-  const { _id } = useSelector((state) => state.user.users || {});
-  console.log(_id && _id);
-  //fetching those created blogs from redux state
-  const { blogs, myBlogs } = useSelector((state) => state.blog || {});
-  console.log(myBlogs && myBlogs);
 
-  // console.log(blogs);
+  //fetching user uuid
+  const { uuid } = useSelector((state) => state.user.users || {});
+  // console.log(uuid && uuid);
+
+  //fetching those created blogs from redux state
+  const { myBlogs } = useSelector((state) => state.blog || {});
+  // console.log(myBlogs && myBlogs);
+
+  const [myBlogsAvailable, setMyBlogsAvailable] = useState(false);
+
   useEffect(() => {
-    if (_id) dispatch(fetchUserCreatedBlogs(_id));
-    console.log(myBlogs);
-  }, [dispatch, _id]);
+    if (uuid) {
+      dispatch(fetchUserCreatedBlogs(uuid));
+    }
+  }, [dispatch, uuid]);
+
+  useEffect(() => {
+    setMyBlogsAvailable(myBlogs.length > 0);
+  }, [myBlogs]);
 
   return (
     <>
       <h1 className="text-gray-500 capitalize font-bold text-3xl sm:text-4xl text-center my-8">
         my blogs
       </h1>
-      <BlogsList blogs_data={myBlogs} />
+      {myBlogsAvailable ? (
+        <BlogsList blogs_data={myBlogs} />
+      ) : (
+        <div className="text-lg text-center col-span-1 sm:col-span-2 lg:col-span-3">
+          No Blogs Available...{" "}
+          {
+            <Link to="../editor" className="text-btnColor">
+              Write One{" "}
+              <i className="fa-regular fa-pen-to-square cursor-pointer"></i>
+            </Link>
+          }
+        </div>
+      )}
     </>
   );
 }
