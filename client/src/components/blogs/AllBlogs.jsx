be3b279a26d98f64b81ad60adadb097f4f1c7125
Anirban Dutta <meanirbandutta22@gmail.com>
2024-10-15 16:51:48 +0530

    all blogs fetch functionality

--- client/src/components/blogs/AllBlogs.jsx
+++ client/src/components/blogs/AllBlogs.jsx
@@ -1,16 +1,21 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchAllBlogs } from "../../features/blog/userBlogSlice";
 import BlogsList from "./BlogList";
+import { Link } from "react-router-dom";
 
 export default function AllBlogs() {
   const dispatch = useDispatch();
   const { blogs, loading, error } = useSelector((state) => state.blog);
 
-  console.log(blogs);
+  // console.log(blogs);
+  const [blogsAvailable, setBlogsAvailable] = useState(false);
 
   useEffect(() => {
-    dispatch(fetchAllBlogs());
+    if (blogs.length > 0) {
+      dispatch(fetchAllBlogs());
+      setBlogsAvailable(!blogsAvailable);
+    }
   }, [dispatch]);
 
   return (
@@ -19,8 +24,19 @@ export default function AllBlogs() {
         Explore our latest articles, guides, and insights on a variety of topics
         to stay informed and inspired
       </h1>
-
-      <BlogsList blogs_data={blogs} loadingState={loading} errors={error} />
+      {blogsAvailable ? (
+        <BlogsList blogs_data={blogs} loadingState={loading} errors={error} />
+      ) : (
+        <div className="text-lg text-center col-span-1 sm:col-span-2 lg:col-span-3">
+          No Blogs Available...{" "}
+          {
+            <Link to="editor" className="text-btnColor">
+              Write One{" "}
+              <i className="fa-regular fa-pen-to-square cursor-pointer"></i>
+            </Link>
+          }
+        </div>
+      )}
     </>
   );
 }
