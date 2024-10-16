4dc260c1db08ed8ae122f7ef2654c018102694b6
Anirban Dutta <meanirbandutta22@gmail.com>
2024-10-16 19:12:00 +0530

    admin route updated (study material)

--- server/src/routes/admin.route.js
+++ server/src/routes/admin.route.js
@@ -7,7 +7,12 @@ const {
   addAdmin,
 } = require("../controllers/admin/adminauth.controller");
 const authHandler = require("../middlewares/authHandler.js");
+const {
+  createTopic,
+} = require("../controllers/admin/study_material.controller.js");
+const upload = require("../utils/uploader.js");
 
+//authentication
 router.route("/add-admin").post(addAdmin);
 router.route("/admin-login").post(loginAdmin);
 router
@@ -16,5 +21,14 @@ router
 router
   .route("/admin/reset-admin-password")
   .post(authHandler({ userType: "admin" }), changeAdminPassword);
+
+//manage study material
+router
+  .route("/admin/study-material/create-topic")
+  .post(
+    authHandler({ userType: "admin" }),
+    upload.single("image"),
+    createTopic
+  );
 
 module.exports = router;
