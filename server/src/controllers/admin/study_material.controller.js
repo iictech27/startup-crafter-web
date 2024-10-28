71cb20e2b661a6e5e671bb0cc2a4b3910d91a29f
Anirban Dutta <meanirbandutta22@gmail.com>
2024-10-28 12:50:27 +0530

    create subtopic, unit, module done

--- server/src/controllers/admin/study_material.controller.js
+++ server/src/controllers/admin/study_material.controller.js
@@ -6,7 +6,9 @@ const {
 } = require("../../models/study_material.model");
 const asyncHandler = require("../../utils/asyncHandler");
 const uploadFileOnCloudinary = require("../../utils/cloudinary");
-const { ApiError } = require("../../utils/customErrorHandler");
+const { ApiError, NotFoundError } = require("../../utils/customErrorHandler");
+// const pdfTextParser = require("../../utils/pdfParser");
+const pdf = require("pdf-parse");
 const ResponseHandler = require("../../utils/responseHandler");
 
 const createTopic = asyncHandler(async (req, res) => {
@@ -49,17 +51,37 @@ const createTopic = asyncHandler(async (req, res) => {
 });
 
 const createSubTopic = asyncHandler(async (req, res) => {
+  const topicId = req.params.topicId;
+  console.log(topicId);
   const { title, description, prerequisites } = req.body;
-  const image = req.file?.path;
+  let image = req.file?.path;
+
+  const topic = await Topic.findOne({ uuid: topicId });
+
+  if (!topic) {
+    throw new NotFoundError("Topic Not Found !");
+  }
 
   if (!title || !description) {
     throw new ApiError(400, "All fields are required !");
   }
 
+  if (!image) {
+    image = "public\\uploads\\dummy_blog.jpg";
+  }
+
+  const cover_image = await uploadFileOnCloudinary(image);
+
+  if (!cover_image) {
+    throw new ApiError(
+      500,
+      "Cover Image not uploaded ! Something went wrong !"
+    );
+  }
   const newSubTopic = await SubTopic.create({
     title,
     description,
-    cover_image: image,
+    cover_image,
     prerequisites,
   });
 
@@ -69,6 +91,14 @@ const createSubTopic = asyncHandler(async (req, res) => {
   if (!createdNewSubTopic) {
     throw new ApiError(500, "Something went wrong");
   }
+
+  await Topic.findByIdAndUpdate(
+    topic._id,
+    {
+      $push: { subtopics: newSubTopic._id },
+    },
+    { new: true }
+  );
 
   return res
     .status(200)
@@ -82,7 +112,16 @@ const createSubTopic = asyncHandler(async (req, res) => {
 });
 
 const createUnit = asyncHandler(async (req, res) => {
+  const subTopicId = req.params.subTopicId;
+  console.log(subTopicId);
+
   const { title, description1, description2 } = req.body;
+
+  const subTopic = await SubTopic.findOne({ uuid: subTopicId });
+
+  if (!subTopic) {
+    throw new NotFoundError("SubTopic Not Found !");
+  }
 
   if (!title || !description1 || !description2) {
     throw new ApiError(400, "All fields are required !");
@@ -99,6 +138,14 @@ const createUnit = asyncHandler(async (req, res) => {
     throw new ApiError(500, "Something went wrong");
   }
 
+  await SubTopic.findByIdAndUpdate(
+    subTopic._id,
+    {
+      $push: { units: newUnit._id },
+    },
+    { new: true }
+  );
+
   return res
     .status(200)
     .json(
@@ -107,6 +154,15 @@ const createUnit = asyncHandler(async (req, res) => {
 });
 
 const createModule = asyncHandler(async (req, res) => {
+  const unitId = req.params.unitId;
+  console.log(unitId);
+
+  const unit = await Unit.findOne({ uuid: unitId });
+
+  if (!unit) {
+    throw new NotFoundError("Unit Not Found !");
+  }
+
   const { title } = req.body;
   const contentFile = req.file?.path;
 
@@ -114,15 +170,32 @@ const createModule = asyncHandler(async (req, res) => {
     throw new ApiError(400, "All fields are required !");
   }
 
+  const content = await pdf(contentFile)
+    .then((result) => {
+      return result.text.toString();
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  console.log(content);
+
   const newModule = await Module.create({
     title,
-    content: contentFile,
+    content,
   });
 
   const createdNewModule = await Module.findById(newModule._id).select("-_id");
   if (!createdNewModule) {
     throw new ApiError(500, "Something went wrong");
   }
+
+  await Unit.findByIdAndUpdate(
+    unit._id,
+    {
+      $push: { modules: newModule._id },
+    },
+    { new: true }
+  );
 
   return res
     .status(200)
