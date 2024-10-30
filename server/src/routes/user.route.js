5fab78959bce2ec3349cef8a995ce2f35be55eee
Anirban Dutta <meanirbandutta22@gmail.com>
2024-10-30 18:41:27 +0530

    idea routes added

--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -26,6 +26,11 @@ const {
   getUnits,
   getModules,
 } = require("../controllers/user/study_material.controller.js");
+const {
+  submitIdea,
+  deleteIdea,
+  getIdeas,
+} = require("../controllers/user/idea.controller.js");
 
 //authentication
 router.route("/user-register").post(registerUser);
@@ -59,11 +64,22 @@ router
   .post(authHandler({ userType: "user" }), unFollowUser);
 
 //idea
+router
+  .route("/user/submit-idea")
+  .post(
+    authHandler({ userType: "user" }),
+    upload.single("document"),
+    submitIdea
+  );
+router
+  .route("/user/delete-idea")
+  .delete(authHandler({ userType: "user" }), deleteIdea);
+router
+  .route("/user/get-ideas")
+  .get(authHandler({ userType: "user" }), getIdeas);
 
 //study material
 router.route("/user/get-all-topics").get(getAllTopics);
 router.route("/user/get-subtopics/:topicId").get(getSubTopics);
-// router.route("/user/get-units/:subTopicId").get(getUnits);
-// router.route("/user/get-modules/:unitId").get(getModules);
 
 module.exports = router;
