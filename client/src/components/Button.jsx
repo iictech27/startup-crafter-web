519cdfc0b3b08f5a8e2997832cf840b6246e8757
Anirban Dutta <meanirbandutta22@gmail.com>
2024-10-30 18:45:46 +0530

    type added and some style changed

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 
 export default function Button({
+  type,
   title,
   absolute,
   responsiveBtn,
@@ -16,8 +17,24 @@ export default function Button({
   return (
     <div className="flex flex-col relative">
       <button
+        type={type}
         onClick={onHandleClick}
-        className={`${btnColor ? btnColor : "bg-btnColor"} rounded-2xl md:rounded-2xl text-sm md:text-lg ${textColor ? textColor : "text-white"} font-semibold shadow-md md:shadow-md cursor-pointer w-max capitalize ${absolute && `flex items-center absolute ${!responsiveBtn ? "-top-10 md:-top-14 right-1 sm:right-0" : "md:-top-14 md:right-0"} rounded-2xl md:rounded-[25px] shadow-none sm:py-3.5 sm:px-8 lg:px-8 font-semibold`} ${large ? "px-4 py-5 md:px-8 lg:px-10" : "px-4 py-2 md:px-6 lg:px-8"} ${small && "text-[10px] md:text-sm px-[8px] py-0.5 sm:py-1 lg:px-2"} transition ease-in-out md:hover:shadow-lg ${className}`}
+        className={`${
+          btnColor ? btnColor : "bg-btnColor"
+        } rounded-2xl md:rounded-2xl text-sm md:text-lg ${
+          textColor ? textColor : "text-white"
+        } font-semibold shadow-md md:shadow-md cursor-pointer w-max capitalize ${
+          absolute &&
+          `flex items-center absolute ${
+            !responsiveBtn
+              ? "-top-10 md:-top-14 right-1 sm:right-0"
+              : "md:-top-14 md:right-0"
+          } rounded-2xl md:rounded-[25px] shadow-none sm:py-3.5 sm:px-8 lg:px-8 font-semibold`
+        } ${
+          large ? "px-4 py-5 md:px-8 lg:px-10" : "px-4 py-2 md:px-6 lg:px-8"
+        } ${
+          small && "text-[10px] md:text-sm px-[8px] py-0.5 sm:py-1 lg:px-2"
+        } transition ease-in-out md:hover:shadow-lg ${className}`}
       >
         {title}
         {children}
