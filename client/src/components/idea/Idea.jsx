fe0909d27cd0589f6e31f1296bc648fa6b8f8957
Anirban Dutta <meanirbandutta22@gmail.com>
2024-10-30 18:42:28 +0530

    admin idea list updated

--- client/src/components/idea/Idea.jsx
+++ client/src/components/idea/Idea.jsx
@@ -2,9 +2,10 @@ import { useNavigate } from "react-router-dom";
 import styles from "./Idea.module.css";
 
 export default function Idea({
-  pdfPath = "#",
-  fileName = "Idea.pdf",
-  ideaDetail = "Tenu Me lav kr da be matlab kr da baho main aa soriyen bas aaj raat ke liye saadi ta desi hai adda sade pe ho jaye fida , subah hone na saam hone na de eek dusre ko hum sone na de mai tera hero haan hero. Tenu Me lav kr da be matlab kr da baho main aa soriyen bas aaj raat ke liye saadi ta desi hai adda sade pe ho jaye fida , subah hone na saam hone na de eek dusre ko hum sone na de mai tera hero haan hero.",
+  filePath,
+  ideaTitle,
+  ideaDescription,
+  ideaOwner,
 }) {
   const navigate = useNavigate();
 
@@ -12,20 +13,20 @@ export default function Idea({
     <>
       <div className={styles.idea}>
         <div className={styles.ideaOwner}>
-          <img src="assets/placeholder.png"></img>
-          <h1>Username</h1>
+          <h1>Idea Title : {ideaTitle} </h1>
+          <h1>{ideaOwner}</h1>
         </div>
 
         <div className={styles.ideaDetail}>
-          <p> {ideaDetail}</p>
+          <p> {ideaDescription}</p>
         </div>
 
         <div className={styles.attachments}>
           <div className={styles.attachment}>
             <span> Attachments</span>
             <a
-              href={pdfPath}
-              download={fileName}
+              href={filePath}
+              download="Idea.pdf"
               className={styles.file_button}
             >
               Idea.pdf
