4959185a6e765f36159c5bf740ef5a7325efd17f
Anirban Dutta <meanirbandutta22@gmail.com>
2024-11-28 01:10:28 +0530

    model for internship

---
+++ server/src/models/internship.model.js
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+
+const internshipSchema = mongoose.Schema({
+  description: {
+    type: String,
+    required: true,
+  },
+
+  company: {
+    type: "String",
+    required: true,
+  },
+
+  stipend: {
+    type: Number,
+    default: 0,
+  },
+
+  domain: {
+    type: String,
+  },
+
+  startDate: {
+    type: String,
+  },
+
+  endDate: {
+    type: String,
+    totalSeats: {
+      type: Number,
+      required: true,
+    },
+
+    vacantSeats: {
+      type: Number,
+      required: true,
+    },
+
+    appliedUsers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+
+    selectedUsers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+});
+
+export const Internship = mongoose.model("Internship", internshipSchema);
